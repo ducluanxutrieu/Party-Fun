7990596984044605a44ec61995102926121430de
=ductrung1298 <55187643+ductrung1298@users.noreply.github.com>
2019-12-10 20:54:48 +0700

    Update user.routes.js
    fix router

--- Party Server/routes/user/user.routes.js
+++ Party Server/routes/user/user.routes.js
@@ -1,46 +1,42 @@
-module.exports = function (app) {
+var app = require('express').Router();
+var controller = require('./user.controllers')
+var auth = require('../authorization')
+//dang nhap user
+app.post('/signin', controller.signin);
 
-    var controller = require('./user.controllers')
-    var auth=require('../authorization')
-    //dang nhap user
-    app.post('/user/signin', controller.signin);
+// dang ki
+app.post('/signup', auth.checkinDataSignup, controller.signup);
 
-    // dang ki
-    app.post('/user/signup', auth.checkinDataSignup, controller.signup);
+//dang xuat
+app.post('/signout', auth.isAuthenticated, controller.signout);
 
-    //dang xuat
-    app.post('/user/signout', auth.isAuthenticated, controller.signout);
+//doi mat khau
+app.post('/changepassword', auth.isAuthenticated, auth.checkinDataChangePassword, controller.changepassword);
 
-    //doi mat khau
-    app.post('/user/changepassword',auth.isAuthenticated, auth.checkinDataChangePassword, controller.changepassword);
+//upload avatar
+app.post('/uploadavatar', auth.isAuthenticated, controller.uploadavatar);
 
-    //upload avatar
-    app.post('/user/uploadavatar',auth.isAuthenticated, controller.uploadavatar);
+//reset password
+app.post('/resetpassword', controller.resetpassword);
 
-    //reset password
-    app.post('/user/resetpassword', controller.resetpassword);
+//reset password confirm
+app.post('/resetconfirm', auth.checkinDataResetPassword, controller.resetconfirm);
 
-    //reset password confirm
-    app.post('/user/resetconfirm', auth.checkinDataResetPassword, controller.resetconfirm);
+//update thong tin user
+app.post('/updateuser', auth.isAuthenticated, auth.checkinDataUpdateUser, controller.updateuser);
 
-    //update thong tin user
-    app.post('/user/updateuser', auth.isAuthenticated, auth.checkinDataUpdateUser, controller.updateuser);
+// get thong tin user
+app.get('/profile', auth.isAuthenticated, controller.profile);
 
-    // get thong tin user
-    app.get('/user/profile', auth.isAuthenticated, controller.profile);
+// nang cap quyen
+app.post('/upgraderole', auth.isAuthenticated, auth.checkinDateUpgraderole, controller.upgraderole);
 
-    // nang cap quyen
-    app.post('/user/upgraderole', auth.isAuthenticated, auth.checkinDateUpgraderole, controller.upgraderole);
+// xoa quyen nhan vien
+app.delete('/demotionrole', auth.isAuthenticated, auth.checkinDateUpgraderole, controller.demotionrole);
 
-    // xoa quyen nhan vien
-    app.delete('/user/demotionrole', auth.isAuthenticated, auth.checkinDateUpgraderole, controller.demotionrole);
+// get danh sach username
 
-    // xem anh
-    app.get('/open_image', controller.open_image);
+app.get('/findusernv', auth.isAuthenticated, auth.isStaff, controller.findusernv);
 
-    // get danh sach username
-
-    app.get('/user/findusernv', auth.isAuthenticated, auth.isStaff, controller.findusernv);
-
-    app.get('/user/finduserkh', auth.isAuthenticated, auth.isStaff, controller.finduserkh);
-}
+app.get('/finduserkh', auth.isAuthenticated, auth.isStaff, controller.finduserkh);
+module.exports = app;
