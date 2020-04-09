b873bedb0a13a139415a57fdd26b79628df16f29
Quang Dong <magenkyou0609@gmail.com>
2020-04-09 15:49:20 +0700

    Add animaton to scrollTop button

--- Party Web/Client/src/app/component/scroll-to-top/scroll-to-top.component.ts
+++ Party Web/Client/src/app/component/scroll-to-top/scroll-to-top.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, Inject, HostListener } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+declare var $: any;
+
 @Component({
   selector: 'scroll-to-top',
   templateUrl: './scroll-to-top.component.html',
   styleUrls: ['./scroll-to-top.component.css']
 })
 
+
 export class ScrollToTopComponent implements OnInit {
+
   isWindowScrolled: boolean;
 
   constructor(@Inject(DOCUMENT) private document: Document) { }
@@ -16,14 +20,19 @@ export class ScrollToTopComponent implements OnInit {
   onWindowScroll() {
     if (this.document.body.scrollTop > 30 || this.document.documentElement.scrollTop > 30) {
       this.isWindowScrolled = true;
+      $('.scroll-top-btn').fadeIn(200);
     } else {
       this.isWindowScrolled = false;
+      $('.scroll-top-btn').fadeOut(200);
     }
   }
 
   scrolltoTop() {
-    this.document.body.scrollTop = 0;
-    this.document.documentElement.scrollTop = 0;
+    // this.document.body.scrollTop = 0;
+    // this.document.documentElement.scrollTop = 0;
+    $("html,body").animate({
+      scrollTop: 0
+    }, 500);
   }
 
   ngOnInit() { }
